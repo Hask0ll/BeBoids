ff986bf1af710fb08ab8cd682916cf33a3d972cd
Hask0ll <dobarat.eu@gmail.com>
2025-02-21 16:13:39 +0100

    Welcome to BeBoids project

---
+++ Source/BeBoids/BeBoids.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BeBoids.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BeBoids, "BeBoids" );
+ 
---
+++ Source/BeBoids/BeBoidsGameMode.cpp
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BeBoidsGameMode.h"
+#include "BeBoidsCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ABeBoidsGameMode::ABeBoidsGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+}
