b9f802ee43f02e2a5412b82f112639e90ec84a5c
Haskoll <Hask0ll>
2025-03-16 15:00:38 +0100

    Cleaned code and Fixed Algo boids

--- Source/BeBoids/Entities/Boids.cpp
+++ Source/BeBoids/Entities/Boids.cpp
@@ -5,38 +5,8 @@ ABoids::ABoids()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
-	CollisionComponent = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionComponent"));
-	RootComponent = CollisionComponent;
-    
-	CollisionComponent->SetSphereRadius(100.0f);
-    
-	CollisionComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
-	CollisionComponent->SetCollisionObjectType(ECC_Pawn);
-	CollisionComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
-	CollisionComponent->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
-	CollisionComponent->SetGenerateOverlapEvents(true);
-
 	BoidsMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BoidsMesh"));
-	BoidsMesh->SetupAttachment(CollisionComponent);
-
-	static ConstructorHelpers::FObjectFinder<UStaticMesh> CubeMeshAsset(TEXT("/Engine/BasicShapes/Cube"));
-	if (CubeMeshAsset.Succeeded())
-	{
-		BoidsMesh->SetStaticMesh(CubeMeshAsset.Object);
-		BoidsMesh->SetRelativeScale3D(FVector(0.5f, 0.5f, 0.5f));
-        
-		BoidsMesh->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
-		BoidsMesh->SetCollisionObjectType(ECC_WorldDynamic);
-		BoidsMesh->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Block);
-	}
-
-	BoidsMesh->SetSimulatePhysics(false);
-    
-	OnActorBeginOverlap.AddDynamic(this, &ABoids::OnBeginOverlap);
-	OnActorEndOverlap.AddDynamic(this, &ABoids::OnEndOverlap);
-	
-	CollisionComponent->SetCollisionObjectType(ECC_GameTraceChannel1);
-	CollisionComponent->SetCollisionResponseToChannel(ECC_GameTraceChannel1, ECR_Overlap);
+	BoidsMesh->SetSimulatePhysics(false); 
 }
 
 void ABoids::BeginPlay()
@@ -50,30 +20,27 @@ void ABoids::Tick(float DeltaTime)
 
 	FindNeighbors();
 	ApplySeparation();
-	ApplyObstacleAvoidance();
+	BoidsMovement(DeltaTime);
 	ApplyAlignment();
-	//ApplyCohesion();
-	//ApplyWander();
-    
-	m_Velocity = m_Velocity.GetClampedToSize(m_MinSpeed, m_MaxSpeed);
-	AddActorWorldOffset(m_Velocity * DeltaTime);
-    
-	if (!m_Velocity.IsNearlyZero())
-	{
-		SetActorRotation(m_Velocity.Rotation());
-	}
+	BoidsMovement(DeltaTime);
+	ApplyCohesion();
+	BoidsMovement(DeltaTime);
+	ApplyObstacleAvoidance();
+	BoidsMovement(DeltaTime);
+}
 
-	FVector SteeringForce = CalculateSteeringForces();
-	m_Velocity += SteeringForce * DeltaTime;
+void ABoids::BoidsMovement(float Deltatime)
+{
 	m_Velocity = m_Velocity.GetClampedToSize(m_MinSpeed, m_MaxSpeed);
-	AddActorWorldOffset(m_Velocity * DeltaTime);
+	AddActorWorldOffset(m_Velocity * Deltatime);
 	SetActorRotation(m_Velocity.Rotation());
 }
+
 
 void ABoids::ApplyObstacleAvoidance()
 {
     FVector Direction = m_Velocity.GetSafeNormal();
-    float MaxDistance = 200.0f;
+    MaxDistance = 200.0f;
     bool ObstacleDetected = false;
     
     TArray<FVector> RayDirections;
@@ -127,9 +94,8 @@ void ABoids::ApplyObstacleAvoidance()
 void ABoids::ApplySeparation()
 {
 	FVector Direction = m_Velocity.GetSafeNormal();
-	float MaxDistance = 100.0f;
     
-	for (ABoids* Neighbor : m_Neighbors)
+	for (ABoids* Neighbor : Neighbors)
 	{
 		FVector SeparationVector = GetActorLocation() - Neighbor->GetActorLocation();
 		float Distance = SeparationVector.Size();
@@ -154,14 +120,14 @@ void ABoids::ApplyAlignment()
 {
 	FVector Direction = m_Velocity.GetSafeNormal();
     
-	if (m_Neighbors.Num() > 0)
+	if (Neighbors.Num() > 0)
 	{
 		FVector AverageDirection = FVector::ZeroVector;
-		for (ABoids* Neighbor : m_Neighbors)
+		for (ABoids* Neighbor : Neighbors)
 		{
 			AverageDirection += Neighbor->m_Velocity.GetSafeNormal();
 		}
-		AverageDirection /= m_Neighbors.Num();
+		AverageDirection /= Neighbors.Num();
         
 		Direction += AverageDirection * m_AlignmentWeight;
 		Direction.Normalize();
@@ -173,85 +139,35 @@ void ABoids::ApplyAlignment()
 
 void ABoids::ApplyCohesion()
 {
-    if (m_Neighbors.Num() == 0)
-        return;
+	if (Neighbors.Num() == 0)
+		return;
+        
+	FVector Centroid = FVector::ZeroVector;
     
-    FVector Direction = m_Velocity.GetSafeNormal();
+	for (AActor* Neighbor : Neighbors)
+	{
+		if (Neighbor)
+		{
+			Centroid += Neighbor->GetActorLocation();
+		}
+	}
     
-    FVector CenterOfMass = FVector::ZeroVector;
-    for (ABoids* Neighbor : m_Neighbors)
-    {
-        CenterOfMass += Neighbor->GetActorLocation();
-    }
-    CenterOfMass /= m_Neighbors.Num();
+	Centroid /= Neighbors.Num();
     
-    FVector ToCenterVector = CenterOfMass - GetActorLocation();
-    float Distance = ToCenterVector.Size();
+	FVector ToCentroid = Centroid - GetActorLocation();
     
-    float MaxDistance = 300.0f;
+	if (!ToCentroid.IsNearlyZero())
+	{
+		ToCentroid.Normalize();
+		ToCentroid *= m_CohesionWeight;
+	}
     
-    if (Distance > 0.0f && Distance < MaxDistance)
-    {
-        float Ratio = Distance / MaxDistance;
-        
-        Direction += ToCenterVector.GetSafeNormal() * Ratio * m_CohesionWeight;
-        
-        if (!Direction.IsNearlyZero())
-        {
-            Direction.Normalize();
-        }
-        
-        float CurrentSpeed = m_Velocity.Size();
-        m_Velocity = Direction * CurrentSpeed;
-    }
-    
-    /*if (GetWorld()->IsPlayInEditor())
-    {
-        DrawDebugLine(GetWorld(), GetActorLocation(), CenterOfMass, 
-                      FColor::Blue, false, -1.0f, 0, 1.0f);
-        DrawDebugSphere(GetWorld(), CenterOfMass, 10.0f, 8, 
-                      FColor::Blue, false, -1.0f, 0, 1.0f);
-    }*/
-}
-
-void ABoids::ApplyWander()
-{
-    FVector Direction = m_Velocity.GetSafeNormal();
-    
-    float WanderStrength = 0.1f;
-    float WanderRate = 0.3f;
-    
-    if (FMath::FRand() < WanderRate)
-    {
-        float RandomAngleYaw = FMath::RandRange(-20.0f, 20.0f);
-        float RandomAnglePitch = FMath::RandRange(-10.0f, 10.0f);
-        
-        FRotator RandomRotation(RandomAnglePitch, RandomAngleYaw, 0.0f);
-        
-        FVector WanderDirection = RandomRotation.RotateVector(Direction);
-        
-        Direction += WanderDirection * WanderStrength * m_WanderWeight;
-        
-        if (!Direction.IsNearlyZero())
-        {
-            Direction.Normalize();
-        }
-        
-        float CurrentSpeed = m_Velocity.Size();
-        m_Velocity = Direction * CurrentSpeed;
-        
-        /*if (GetWorld()->IsPlayInEditor())
-        {
-            DrawDebugLine(GetWorld(), GetActorLocation(), 
-                          GetActorLocation() + WanderDirection * 50.0f, 
-                          FColor::Yellow, false, 0.2f, 0, 1.0f);
-        }*/
-    }
+	m_Velocity += ToCentroid;
 }
 
 void ABoids::FindNeighbors()
 {
-	m_Neighbors.Empty();
+	Neighbors.Empty();
 
 	TArray<AActor*> AllBoids;
 	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ABoids::StaticClass(), AllBoids);
@@ -266,162 +182,8 @@ void ABoids::FindNeighbors()
 
 			if (Distance <= m_PerceptionRadius)
 			{
-				m_Neighbors.Add(BoidRef);
+				Neighbors.Add(BoidRef);
 			}
 		}
-	}
-}
-
-FVector ABoids::CalculateSteeringForces()
-{
-	FVector SeparationForce = CalculateSeparation();
-	FVector AlignmentForce = CalculateAlignment();
-	FVector CohesionForce = CalculateCohesion();
-	FVector AvoidanceForce = CalculateObstacleAvoidance();
-	FVector WanderForce = CalculateWanderForce();
-    
-	return SeparationForce * m_SeparationWeight + 
-		   AlignmentForce * m_AlignmentWeight + 
-		   CohesionForce * m_CohesionWeight + 
-		   AvoidanceForce * m_AvoidanceWeight + WanderForce * m_WanderWeight;
-}
-
-FVector ABoids::CalculateSeparation()
-{
-	FVector SeparationDirection = FVector::ZeroVector;
-	float MaxDistance = m_PerceptionRadius;
-    
-	if (m_Neighbors.Num() == 0)
-		return SeparationDirection;
-    
-	for (ABoids* Neighbor : m_Neighbors)
-	{
-		FVector DifferenceVector = GetActorLocation() - Neighbor->GetActorLocation();
-		float Distance = DifferenceVector.Size();
-        
-		if (Distance > 0.0f && Distance < MaxDistance)
-		{
-			float Ratio = Distance / MaxDistance;
-			SeparationDirection += DifferenceVector * Ratio;
-		}
-	}
-    
-	if (!SeparationDirection.IsNearlyZero())
-	{
-		SeparationDirection.Normalize();
-	}
-    
-	return SeparationDirection;
-}
-
-FVector ABoids::CalculateAlignment()
-{
-	FVector AlignmentForce = FVector::ZeroVector;
-
-	for (ABoids* Neighbor : m_Neighbors)
-	{
-		AlignmentForce += Neighbor->m_Velocity;
-	}
-
-	if (m_Neighbors.Num() > 0)
-	{
-		AlignmentForce /= m_Neighbors.Num();
-		AlignmentForce -= m_Velocity;
-	}
-
-	return AlignmentForce;
-}
-
-FVector ABoids::CalculateCohesion()
-{
-	FVector CohesionForce = FVector::ZeroVector;
-
-	for (ABoids* Neighbor : m_Neighbors)
-	{
-		CohesionForce += Neighbor->GetActorLocation();
-	}
-
-	if (m_Neighbors.Num() > 0)
-	{
-		CohesionForce /= m_Neighbors.Num();
-		CohesionForce -= GetActorLocation();
-	}
-
-	return CohesionForce;
-}
-
-FVector ABoids::CalculateObstacleAvoidance() const
-{
-    FVector AvoidanceDirection = FVector::ZeroVector;
-    float MaxDistance = 200.0f;
-    
-    TArray<FVector> RayDirections;
-    
-    RayDirections.Add(GetActorForwardVector());
-    
-    FRotator LeftRot(0, -30, 0);
-    FRotator RightRot(0, 30, 0);
-    RayDirections.Add(LeftRot.RotateVector(GetActorForwardVector()));
-    RayDirections.Add(RightRot.RotateVector(GetActorForwardVector()));
-    
-    for (const FVector& RayDir : RayDirections)
-    {
-        FHitResult HitResult;
-        FCollisionQueryParams CollisionParams;
-        CollisionParams.AddIgnoredActor(this);
-        
-        FVector Start = GetActorLocation();
-        FVector End = Start + RayDir * MaxDistance;
-        
-        bool bHit = GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECC_Visibility, CollisionParams);
-        
-        if (bHit)
-        {
-            FVector DifferenceVector = Start - HitResult.ImpactPoint;
-            float Distance = DifferenceVector.Size();
-            
-            float Ratio = 1.0f - (Distance / MaxDistance);
-            
-            AvoidanceDirection += DifferenceVector.GetSafeNormal() * Ratio * Ratio;
-            
-            /*if (GetWorld()->IsPlayInEditor())
-            {
-                DrawDebugLine(GetWorld(), Start, HitResult.ImpactPoint, FColor::Red, false, -1.0f, 0, 2.0f);
-                DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, 10.0f, 8, FColor::Orange, false, -1.0f, 0, 1.0f);
-            }*/
-        }
-    }
-    
-    if (!AvoidanceDirection.IsNearlyZero())
-    {
-        AvoidanceDirection.Normalize();
-    }
-    
-    return AvoidanceDirection;
-}
-
-FVector ABoids::CalculateWanderForce()
-{
-	float RandomAngle = FMath::RandRange(-30.0f, 30.0f);
-	FRotator Rotation = FRotator(0, RandomAngle, 0);
-	FVector WanderDirection = Rotation.RotateVector(m_Velocity.GetSafeNormal());
-    
-	return WanderDirection * 0.1f;
-}
-
-void ABoids::OnBeginOverlap(AActor* OverlappedActor, AActor* OtherActor)
-{
-	ABoids* OtherBoids = Cast<ABoids>(OtherActor);
-	if (OtherBoids && OtherBoids != this)
-	{
-		m_Neighbors.AddUnique(OtherBoids);
-	}
-}
-
-void ABoids::OnEndOverlap(AActor* OverlappedActor, AActor* OtherActor)
-{
-	if (ABoids* OtherBoids = Cast<ABoids>(OtherActor))
-	{
-		m_Neighbors.Remove(OtherBoids);
 	}
 }
--- Source/BeBoids/Entities/Boids.h
+++ Source/BeBoids/Entities/Boids.h
@@ -17,102 +17,48 @@ class BEBOIDS_API ABoids : public AActor
 	GENERATED_BODY()
 
 public:
-	// Constructor
 	ABoids();
 
 	// Mesh component for the boid
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category=Mesh)
 	UStaticMeshComponent* BoidsMesh;
 
-	// Collision component for the boid
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category=Collision)
-	USphereComponent* CollisionComponent;
-
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Finds neighboring boids within a certain radius
 	UFUNCTION()
 	void FindNeighbors();
 
-	// Called when another actor begins to overlap with this actor
-	UFUNCTION()
-	void OnBeginOverlap(AActor* OverlappedActor, AActor* OtherActor);
-
-	// Called when another actor ends overlap with this actor
-	UFUNCTION()
-	void OnEndOverlap(AActor* OverlappedActor, AActor* OtherActor);
-
-	// Array of neighboring boids
 	UPROPERTY()
-	TArray<ABoids*> m_Neighbors;
+	TArray<ABoids*> Neighbors;
 
-private:
-	// Applies separation behavior to the boid
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Boids")
+	float m_PerceptionRadius = 500.0f;
+	
 	void ApplySeparation();
-
-	// Applies obstacle avoidance behavior to the boid
 	void ApplyObstacleAvoidance();
-
-	// Applies alignment behavior to the boid
 	void ApplyAlignment();
-
-	// Applies cohesion behavior to the boid
 	void ApplyCohesion();
-
-	// Applies wandering behavior to the boid
-	void ApplyWander();
-
-	// Calculates the wander force for the boid
-	FVector CalculateWanderForce();
-
-	// Calculates the steering forces for the boid
-	FVector CalculateSteeringForces();
-
-	// Calculates the separation force for the boid
-	FVector CalculateSeparation();
-
-	// Calculates the alignment force for the boid
-	FVector CalculateAlignment();
-
-	// Calculates the cohesion force for the boid
-	FVector CalculateCohesion();
-
-	// Calculates the obstacle avoidance force for the boid
-	FVector CalculateObstacleAvoidance() const;
-
-	// Current velocity of the boid
+	void BoidsMovement(float Deltatime);
+	
 	FVector m_Velocity;
-
-	// Maximum speed of the boid
-	float m_MaxSpeed = 500.0f;
-
-	// Minimum speed of the boid
+	float MaxDistance = 100.0f;
+	float m_MaxSpeed = 1000.0f;
 	float m_MinSpeed = 200.0f;
 
-	// Perception radius for detecting neighbors
-	float m_PerceptionRadius = 500.0f;
-
-	// Weight for alignment behavior
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Boids")
 	float m_AlignmentWeight = 1.0f;
 
-	// Weight for cohesion behavior
-	float m_CohesionWeight = 1.0f;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Boids")
+	float m_CohesionWeight = 5.0f;
 
-	// Weight for separation behavior
-	float m_SeparationWeight = 1.0f;
-
-	// Radius for separation behavior
-	float m_SeparationRadius = 150.0f;
-
-	// Weight for obstacle avoidance behavior
-	float m_AvoidanceWeight = 1.0f;
-
-	// Weight for wandering behavior
-	FVector m_WanderWeight;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Boids")
+	float m_SeparationWeight = 0.2f;
+	
+	float m_AvoidanceWeight = 5.0f;
+	
 };
